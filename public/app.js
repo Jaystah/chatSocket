104a8087c3d6a7ef569a7060bb1cb91327007215
Jay Doerga <jay@jjdeveloping.nl>
2020-12-19 10:25:45 +0100

    jQuery

--- public/app.js
+++ public/app.js
@@ -2,74 +2,76 @@ const socket = io();
 
 let allpeople = [];
 let name2;
-socket.on('setName',obj=>{
+socket.on('setName', obj => {
     document.getElementById('name').innerText = `Name: ${obj}`;
 })
-do{
+do {
     name2 = prompt('Geef je naam')
-}while(!name2);
+} while (!name2);
 
-M.toast({html: `Welcome ${name2}!`, classes: 'rounded'});
-setTimeout(socket.emit('nameConfig',name2),4000);
-const sendMessage = (msg)=>{
-    if(msg){
-document.getElementById('msg').value = "";
-display({
-    message: msg,
-    name2: "You"
-},'me')
+M.toast({ html: `Welcome ${name2}!`, classes: 'rounded' });
+setTimeout(socket.emit('nameConfig', name2), 4000);
+const sendMessage = (msg) => {
+    if (msg) {
+        document.getElementById('msg').value = "";
+        display({
+            message: msg,
+            name2: "You"
+        }, 'me')
 
-let mess= {
-    message : msg,
-    name2
+        let mess = {
+            message: msg,
+            name2
+        }
+
+        socket.emit('newMsg', mess);
+    }
 }
 
-socket.emit('newMsg',mess);
-}
-}
 
-
-socket.on('sendToAll',msg=>{
-    display(msg,'other');
+socket.on('sendToAll', msg => {
+    display(msg, 'other');
 })
-socket.on('left',msg=>{
+
+socket.on('left', msg => {
     left(msg);
 })
 
-setInterval(()=>{
-socket.emit("getCounter")
-},100);
+setInterval(() => {
+    socket.emit("getCounter")
+}, 100);
 
 clearInterval();
 
-socket.on('onliners',amount=>{
-  amount == 1 ? document.getElementById("count").innerHTML = amount + " chatter is online" :    document.getElementById("count").innerHTML = amount + " people are online"
+socket.on('onliners', amount => {
+    amount == 1 ? document.getElementById("count").innerHTML = amount + " chatter is online" : document.getElementById("count").innerHTML = amount + " people are online"
 })
 
 
-socket.on('allPeople',object=>{
-    if(allpeople != object){
+socket.on('allPeople', object => {
+    if (allpeople != object) {
         clearBox('people');
-    object.forEach((i)=>{
-       let p = document.createElement('li');
-       p.innerHTML = i;
-       p.className = "collection-item";
-       document.getElementById('people').prepend(p);
-    })
-}
+        object.forEach((i) => {
+            let p = document.createElement('li');
+            p.innerHTML = i;
+            p.className = "collection-item";
+            document.getElementById('people').prepend(p);
+        })
+    }
 })
 
-socket.on('newJoin',obj=>{
-    M.toast({html: `${obj} joined the party!`, classes: 'rounded'});
+socket.on('newJoin', obj => {
+    M.toast({ html: `${obj} joined the party!`, classes: 'rounded' });
 })
 
-const display = (msg,type)=>{
+const display = (msg, type) => {
     let place = document.getElementById('messages');
     let paragraph = document.createElement("P");
-    paragraph.innerHTML = type == 'me' ? "<b class='msgName'>"+msg.name2 + "</b> - <span class='msgMessage'>" + msg.message + "</span>" : "<b style='color: red;'>"+msg.name2 + "</b> - <span class='msgMessage'>" + msg.message + "</span>";
-    place.prepend(paragraph);
+    paragraph.innerHTML = type == 'me' ? "<b class='msgName'>" + msg.name2 + "</b> - <span class='msgMessage'>" + msg.message + "</span>" : "<b style='color: red;'>" + msg.name2 + "</b> - <span class='msgMessage'>" + msg.message + "</span>";
+    paragraph.style.display = "none";
+    $(paragraph).prependTo('#messages').show('slow');
 }
-const left = (msg)=>{
+const left = (msg) => {
     let place = document.getElementById('messages');
     let paragraph = document.createElement("P");
     paragraph.innerHTML = msg + " left the party";
@@ -79,60 +81,68 @@ const left = (msg)=>{
 
 document.addEventListener('keyup', (e) => {
     e.preventDefault();
-    if (e.keyCode === 13)      
+    if (e.keyCode === 13)
         sendMessage(document.getElementById('msg').value);
-  });
+});
 
-  function clearBox(elementID) { 
-    var div = document.getElementById(elementID); 
-      
-    while(div.firstChild) { 
-        div.removeChild(div.firstChild); 
-    } 
-} 
+const clearBox = (elementID) => {
+    var div = document.getElementById(elementID);
 
-socket.on('playGhetto',()=>{
-   if(document.getElementById('ghetto').paused) 
-    playGhetto();
+    while (div.firstChild) {
+        div.removeChild(div.firstChild);
+    }
+}
+
+socket.on('playGhetto', () => {
+    if (document.getElementById('ghetto').paused)
+        playGhetto();
 })
-socket.on('pauseGhetto',()=>{
-   if(!document.getElementById('ghetto').paused) 
-    pauseGhetto();
+socket.on('pauseGhetto', () => {
+    if (!document.getElementById('ghetto').paused)
+        pauseGhetto();
 })
-socket.on('playKurt',()=>{
-   if(document.getElementById('kurt').paused) 
-    playKurt();
+socket.on('playKurt', () => {
+    if (document.getElementById('kurt').paused)
+        playKurt();
 })
-socket.on('pauseKurt',()=>{
-   if(!document.getElementById('kurt').paused) 
-    pauseKurt();
+socket.on('pauseKurt', () => {
+    if (!document.getElementById('kurt').paused)
+        pauseKurt();
 })
 
-const playGhetto = ()=>{
+const playGhetto = () => {
     document.getElementById('ghetto').play();
-    socket.emit('getTime',(document.getElementById('ghetto').currentTime,document.getElementById('kurt').currentTime))
+    socket.emit('getTime', (document.getElementById('ghetto').currentTime, document.getElementById('kurt').currentTime))
     socket.emit('playGhetto');
     document.getElementById('playKurtButton').classList.add('disabled')
 
 }
-const pauseGhetto = ()=>{
+const pauseGhetto = () => {
     document.getElementById('ghetto').pause();
     socket.emit('pauseGhetto')
     document.getElementById('playKurtButton').classList.remove('disabled')
 
 }
-const playKurt = ()=>{
+const playKurt = () => {
     document.getElementById('kurt').play();
-    socket.emit('getTime',(document.getElementById('ghetto').currentTime,document.getElementById('kurt').currentTime))
+    socket.emit('getTime', (document.getElementById('ghetto').currentTime, document.getElementById('kurt').currentTime))
     socket.emit('playKurt')
     document.getElementById('playGhettoButton').classList.add('disabled')
 }
-const pauseKurt = ()=>{
+const pauseKurt = () => {
     document.getElementById('kurt').pause();
     socket.emit('pauseKurt')
     document.getElementById('playGhettoButton').classList.remove('disabled')
 }
 
+
+
+
+if(a>b){
+    a = 1;
+}else{
+    a = 2;
+}
 
 
 
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT =  process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 
 app.use(express.static(path.join(__dirname, 'public')))
@@ -14,37 +14,37 @@ let people = [];
 let timeBitteGeh = 0;
 let timeKurt = 0;
 const io = require('socket.io')(http);
-io.on('connect',socket=>{
+io.on('connect', socket => {
     counter++;
-    io.emit('onliners',counter);
-    socket.emit('onliners',counter);
-    console.log(timeBitteGeh,timeKurt);
-    socket.on('nameConfig',(e)=>{
+    io.emit('onliners', counter);
+    socket.emit('onliners', counter);
+    console.log(timeBitteGeh, timeKurt);
+    socket.on('nameConfig', (e) => {
         socket.name = e;
 
-        socket.emit("setName",e);
+        socket.emit("setName", e);
 
-        socket.broadcast.emit('newJoin',e);
+        socket.broadcast.emit('newJoin', e);
 
         people.push(e);
 
-        io.emit('allPeople',people)
+        io.emit('allPeople', people)
     })
 
-    socket.on('getCounter',()=>{
-        socket.emit('onliners',counter);
+    socket.on('getCounter', () => {
+        socket.emit('onliners', counter);
     })
 
-    socket.on('newMsg',msg=>{
+    socket.on('newMsg', msg => {
         console.log(msg);
-        socket.broadcast.emit('sendToAll',msg);
+        socket.broadcast.emit('sendToAll', msg);
     })
-    socket.on('disconnect', function(){
+    socket.on('disconnect', function () {
         counter--;
-        socket.broadcast.emit('left',socket.name ? socket.name :'Someone');
-        io.emit('onliners',counter)
-        people.splice(people.indexOf(socket.name),1)
-        io.emit('allPeople',people);
+        socket.broadcast.emit('left', socket.name ? socket.name : 'Someone');
+        io.emit('onliners', counter)
+        people.splice(people.indexOf(socket.name), 1)
+        io.emit('allPeople', people);
     });
 
     // io.on('getTime',(bitte,kurt)=>{
@@ -53,16 +53,16 @@ io.on('connect',socket=>{
     //     console.log(timeBitteGeh,timeKurt)
     // })
 
-    socket.on('playGhetto',()=>{
+    socket.on('playGhetto', () => {
         socket.broadcast.emit('playGhetto')
     })
-    socket.on('pauseGhetto',()=>{
+    socket.on('pauseGhetto', () => {
         socket.broadcast.emit('pauseGhetto')
     })
-    socket.on('playKurt',()=>{
+    socket.on('playKurt', () => {
         socket.broadcast.emit('playKurt')
     })
-    socket.on('pauseKurt',()=>{
+    socket.on('pauseKurt', () => {
         socket.broadcast.emit('pauseKurt')
     })
 })
@@ -71,6 +71,6 @@ io.on('connect',socket=>{
 
 
 
-http.listen(PORT,()=>{
+http.listen(PORT, () => {
     console.log('Server running on', PORT);
 })
